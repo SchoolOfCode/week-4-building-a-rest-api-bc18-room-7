2bf348a45b1b148d27c8333407b419dbf7933d15
Jacob Smith <183931179+SmithyJ98@users.noreply.github.com>
2024-12-17 14:39:17 +0000

    Completed up to ticket 4

--- app.js
+++ app.js
@@ -16,6 +16,30 @@ app.get("/", function (req, res) {
   res.send("Welcome to the inspirational quotes API");
 });
 
+//listen for a get request
+//the route will be /quotes
+//when we recieve a request
+//get all quotes
+//respond with all quotes
+
+app.get("/quotes", async function (req, res) {
+  const quotes = await getQuotes();
+  res.json(quotes);
+});
+
+//listen for a get request
+//route will be /quotes/:id
+//when we recieve a request we want
+//get the id from the request
+//use the id to find the right quote
+//respond with the quotes:
+
+app.get("/quotes/:id", async function (req, res) {
+  const id = req.params.id;
+  const quote = await getQuoteByID(id);
+  res.json(quote);
+});
+
 app.listen(PORT, function () {
   console.log(`Server is now listening on http://localhost:${PORT}`);
 });
